b71e6673fdce0099eb3d7b99022712b4a74dc120
Shmulya Irish's <human.of.light@gmail.com>
2017-05-16 18:47:19 +0300

    add list of masters

--- fetch.py
+++ fetch.py
@@ -1,9 +1,9 @@
 import subprocess, re
 
-MASTERIP = '192.168.0.1'
+MASTERIP = ['192.168.0.1','192.168.0.1']
 REMOTE_USERNAME = 'control'
 PATH_TO_MASTER_ZONEFILE = '/etc/bind/named.conf.local'
-PATH_TO_SLAVE_ZONEFILE = '/etc/bind/named.conf.local'
+PATH_TO_SLAVE_ZONEFILE = '/etc/bind/'
 
 
 def get_master_zones(masterip, username, masterpath):
@@ -37,7 +37,7 @@ def new_zonefile(newzones, pathtozonefile, master):
 
     for i in newzones:
         fn = re.search('"\S+"', i).group(0)[1:-1]
-        strng = '%s {\n	type slave;\n	file "db.%s";\n		masters { %s; };\n};\n' % (i, fn, master)
+        strng = '%s {\n	type slave;\n	file "/var/cache/bind/db.%s";\n		masters { %s; };\n};\n' % (i, fn, master)
         reszonestr = reszonestr + strng
     open(pathtozonefile, 'w').write(reszonestr)
     subprocess.call("rndc reload", shell=True)
@@ -45,14 +45,15 @@ def new_zonefile(newzones, pathtozonefile, master):
 
 def __main__():
 
-    masterzone = get_master_zones(MASTERIP, REMOTE_USERNAME, PATH_TO_MASTER_ZONEFILE)
-    slavezone = get_slave_zones(PATH_TO_SLAVE_ZONEFILE)
-    newslave = match_append(masterzone, slavezone)
+    for mstr in MASTERIP:
+        masterzone = get_master_zones(mstr, REMOTE_USERNAME, PATH_TO_MASTER_ZONEFILE)
+        slavezone = get_slave_zones(PATH_TO_SLAVE_ZONEFILE+mstr+'.conf')
+        newslave = match_append(masterzone, slavezone)
 
-    if newslave == 'same':
-        print 'No new zonefiles'
-    else:
-        new_zonefile(newslave, PATH_TO_SLAVE_ZONEFILE, MASTERIP)
+        if newslave == 'same':
+            print 'No new zonefiles'
+        else:
+            new_zonefile(newslave, PATH_TO_SLAVE_ZONEFILE+mstr+'.conf', mastr)
 
 
 __main__()
